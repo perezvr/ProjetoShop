a4888d3273f911ce93f1fdc789d61d62c4af2aad
Renan <Renan@DESKTOP-GLNANS6>
2020-04-05 00:52:37 -0300

    Config database

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: "Hello Rcoket" });
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.put('/users', UserController.update);
 
 export default routes;
