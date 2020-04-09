4b6c04077b8830705f069eabf1d213c5e9b99fc0
Renan Perez <perez.vr@gmail.com>
2020-04-09 00:43:32 -0300

    ajuste

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,6 +1,6 @@
 export default {
   // MD5 da chave gerada pela aplicação gerado a partir de => gobarberrocketseatnode2
   secret: 'f29618255c309de4469993cce24286ea',
-  // Tempo de expiração do token
+  // Tempo de expiração do token a partir da data de criação
   expiresIn: '7d',
 };
